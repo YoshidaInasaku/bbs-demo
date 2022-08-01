361d07a5380702c013119b20e13acf03497c1aeb
吉田稲作 <yoshidanoinasaku@gmail.com>
2022-08-01 16:12:14 +0900

    Spring Securityの設定

---
+++ src/main/java/com/yoshidainasaku/output/bbsdemo/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.yoshidainasaku.output.bbsdemo.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(login -> login
+                        .loginProcessingUrl("/login")
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login"))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .antMatchers("/h2-console/**").permitAll()  // h2コンソールにアクセス可能にする
+                        .mvcMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        // h2のデバッグ用
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
