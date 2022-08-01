74bf1f428dcafce2b573973cdce888cb6a5972ad
吉田稲作 <yoshidanoinasaku@gmail.com>
2022-08-01 15:35:19 +0900

    プロジェクトの作成

---
+++ src/test/java/com/yoshidainasaku/output/bbsdemo/BbsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshidainasaku.output.bbsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
