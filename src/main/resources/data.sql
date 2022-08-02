f80b913f6a9866d269d84f67791a38912f47dcfe
吉田稲作 <yoshidanoinasaku@gmail.com>
2022-08-02 12:49:18 +0900

    テーブル初期化時に、テーブルに追加するデータの更新

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- password = "general"
 INSERT INTO users(user_id, user_name, password, email, existence)
-    VALUES('tanaka', '田中一般', '$2a$10$6fPXYK.C9rCWUBifuqBIB.GRNU.nQtBpdzkkKis8ETaKVKxNo/ltO', 'tanaka@example.com', TRUE);
+    VALUES('tanaka', '田中一般', '$2a$10$6fPXYK.C9rCWUBifuqBIB.GRNU.nQtBpdzkkKis8ETaKVKxNo/ltO', 'tanaka@example.com', true);
 -- password = "admin"
 INSERT INTO users(user_id, user_name, password, email, existence)
-    VALUES('morita', '森田管理', '$2a$10$SJTWvNl16fCU7DaXtWC0DeN/A8IOakpCkWWNZ/FKRV2CHvWElQwMS', 'morita@example.com', TRUE);
+    VALUES('morita', '森田管理', '$2a$10$SJTWvNl16fCU7DaXtWC0DeN/A8IOakpCkWWNZ/FKRV2CHvWElQwMS', 'morita@example.com', true);
 
 INSERT INTO roles(id, role_name) VALUES(1, 'ROLE_GENERAL');
 INSERT INTO roles(id, role_name) VALUES(2, 'ROLE_ADMIN');
