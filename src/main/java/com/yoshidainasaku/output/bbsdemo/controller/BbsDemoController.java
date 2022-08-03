5f348e27edec001daf4921a4e325b46afd322d02
吉田稲作 <yoshidanoinasaku@gmail.com>
2022-08-03 11:43:44 +0900

    リポジトリクラスをDIして、ホーム画面に投稿されたコンテンツ一覧を表示さできるように変更

--- src/main/java/com/yoshidainasaku/output/bbsdemo/controller/BbsDemoController.java
+++ src/main/java/com/yoshidainasaku/output/bbsdemo/controller/BbsDemoController.java
@@ -1,17 +1,32 @@
 package com.yoshidainasaku.output.bbsdemo.controller;
 
+import com.yoshidainasaku.output.bbsdemo.persistence.entity.Content;
+import com.yoshidainasaku.output.bbsdemo.persistence.repository.ContentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class BbsDemoController {
+    private final ContentRepository contentRepository;
+
+    @Autowired
+    public BbsDemoController(ContentRepository contentRepository) {
+        this.contentRepository = contentRepository;
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        List<Content> contentList = contentRepository.findAll();
+        model.addAttribute("contentList", contentList);
         return "home";
     }
 }
