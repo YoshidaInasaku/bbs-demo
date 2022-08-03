d39f03b604f8fe9cf9101344e8c0268ff3568d3d
吉田稲作 <yoshidanoinasaku@gmail.com>
2022-08-03 11:34:17 +0900

    コンポーネント名の修正

--- src/main/java/com/yoshidainasaku/output/bbsdemo/persistence/entity/Content.java
+++ src/main/java/com/yoshidainasaku/output/bbsdemo/persistence/entity/Content.java
@@ -2,4 +2,4 @@ package com.yoshidainasaku.output.bbsdemo.persistence.entity;
 
 import java.sql.Timestamp;
 
-public record Content(String textContent, Timestamp updated_at, String userId, String userName) {}
+public record Content(String textContent, Timestamp updatedAt, String userId, String userName) {}
